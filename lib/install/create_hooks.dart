42822c56b59aabfd862da7de94e59981ad9ea892
殷亮 <yinliang@wshifu.com>
2023-07-26 16:31:44 +0800

    <>[]<路径测试>

--- lib/install/create_hooks.dart
+++ lib/install/create_hooks.dart
@@ -23,7 +23,7 @@ class CreateHooks {
     }
 
     ///获取插件包路径
-    targetPath = Utils.getOwnPath() ?? 'hooks/git_hooks.dart';
+    targetPath = (Utils.getOwnPath() ?? '') + 'hooks/git_hooks.dart';
     var relativePath = '${_rootDir}/${targetPath}';
     var hookFile = File(Utils.uri(targetPath));
     var logger = Logger.standard();
