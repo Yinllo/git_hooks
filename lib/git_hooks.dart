7c583b6451769fe4a95f0c641fe9eb453645f67a
殷亮 <yinliang@wshifu.com>
2023-07-26 16:47:35 +0800

    <>[]<路径测试>

--- lib/git_hooks.dart
+++ lib/git_hooks.dart
@@ -1,5 +1,5 @@
 library git_hooks;
 
-export 'package:git_hooks/utils/type.dart';
-export 'package:git_hooks/runtime/git_hooks.dart';
+export './runtime/git_hooks.dart';
+export './utils/type.dart';
 export './utils/utils.dart';
